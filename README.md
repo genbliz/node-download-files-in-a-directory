de31fe0567463cd444149fa7e520787e2642a8ff
Chris Uzoh <genbliz@gmail.com>
2020-03-27 14:13:14 +0100

    improvements and docs

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # node-download-files-in-a-directory
-download files in a directory
+## download files in a directory
+### usage
+- clone the repository: \
+``` git clone https://github.com/genbliz/node-download-files-in-a-directory.git ``` \
+``` cd node-download-files-in-a-directory ```
+- create a directory named ```dl``` at the root of the server and copy all the files you want to make available for download into into it.
+- install packages: \
+``` npm install ```
+- start server: \
+``` npm run start ```
+- open your browser: http://localhost:8081
--- index.js
+++ index.js
@@ -4,46 +4,71 @@ const path = require("path");
 const http = require("http");
 const express = require("express");
 const app = express();
-
 const server = http.createServer(app);
 const PORT = 8081;
 const downloadDirectory = `${__dirname}/dl`;
+/**
+ *
+ * @param {string} baseDownloadlDir
+ * @param {string[]} fileNamesArray
+ * @returns {Promise<string[]>}
+ */
+function readDownloadFileDir(baseDownloadlDir, fileNamesArray = []) {
+  return new Promise((resolve, reject) => {
+    fs.readdir(baseDownloadlDir, (err, files) => {
+      if (err) {
+        reject(`Error occoured`);
+        throw err;
+      }
+      fileNamesArray = files
+        .map(file => {
+          return path.join(baseDownloadlDir, file);
+        })
+        .filter(file => {
+          return fs.statSync(file).isFile();
+        })
+        .map(filePath => {
+          console.log("%s", filePath);
+          // return path.basename(file);
+          const neededFilePath = filePath.split(baseDownloadlDir)[1];
+          return neededFilePath.startsWith("/")
+            ? neededFilePath.slice(1)
+            : neededFilePath;
+        });
+      resolve(fileNamesArray);
+    });
+  });
+}
 
 app.get("/", (req, res) => {
   if (!fs.existsSync(downloadDirectory)) {
     res.send(`<h2>No file(s) found</h2>`);
     return;
   }
-  fs.readdir(downloadDirectory, function(err, files) {
-    if (err) {
-      res.send(`<h2>Error occoured</h2>`);
-      throw err;
-    }
-    const fileNamesArray = files
-      .map(function(file) {
-        return path.join(downloadDirectory, file);
-      })
-      .filter(function(file) {
-        return fs.statSync(file).isFile();
-      })
-      .map(function(file) {
-        console.log("%s (%s)", file, path.extname(file));
-        return path.basename(file);
-      });
-
-    if (fileNamesArray.length) {
-      const html = [];
-      html.push(`<h2>List of Downloads:</h2>`);
-      html.push(`<ul>`);
-      fileNamesArray.forEach(file => {
-        html.push(`<li><a href="download/?fileName=${file}">${file}</a></li>`);
-      });
-      html.push(`</ul>`);
-      res.send(html.join("\n"));
-    } else {
-      res.send(`<h2>No file(s) found</h2>`);
-    }
-  });
+  readDownloadFileDir(downloadDirectory)
+    .then(fileNamesArray => {
+      if (fileNamesArray.length) {
+        const html = [];
+        html.push(`<h2>List of Downloads:</h2>`);
+        html.push(`<ul>`);
+        fileNamesArray.forEach(file => {
+          html.push(
+            `<li><a href="download/?fileName=${file}">${file}</a></li>`
+          );
+        });
+        html.push(`</ul>`);
+        res.send(html.join("\n"));
+      } else {
+        res.send(`<h2>No file(s) found</h2>`);
+      }
+    })
+    .catch(err => {
+      if (typeof err === "string") {
+        res.send(`<h2>${err}</h2>`);
+      } else {
+        res.send(`<h2>Error occured</h2>`);
+      }
+    });
 });
 
 app.get("/download", (req, res) => {
